c35c8dc99350a45a65b9311bbbe3c445c6bc93d7
DS-FAU <42653001+ds-fau-ck@users.noreply.github.com>
2024-04-18 08:10:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md","r", encoding="utif-8") as f:
     long_discriptions=f.read()
 
 REPO_NAME="mlflow-project-template"
-AUTHOR_USER_NAME="TUCchkul"
+AUTHOR_USER_NAME="ds-fau-ck"
 SRC_REPO="src"
 LIST_OF_REQUIREMENTS=[]
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author="Kulakirti Chakma",
+    author="K C",
     descriptions="A small project for mlflow",
     long_discriptions=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTOR_USER_NAME}/{REPO_NAME}",
-    autor_email="kirticse.chakma869@gmail.com",
     package=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
